6bae783c22f6d8509eb104d722d86780a06a1e49
zar4za <zar4z4@yandex.ru>
2023-03-08 00:43:32 +0300

    feat: game service
    Сервис для управления играми.

---
+++ TicTacToe/Services/IGameService.cs
@@ -0,0 +1,15 @@
+﻿using TicTacToe.Models;
+
+namespace TicTacToe.Services
+{
+    public interface IGameService
+    {
+        Guid CreateGame(int crossPlayerId, int circlePlayerId);
+
+        IReadOnlyCollection<Mark>? GetField(Guid guid);
+
+        void MakeMove(Guid guid, int playerid, int row, int column);
+        
+        int? GetWinner(Guid guid);
+    }
+}
